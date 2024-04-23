7f696dff32304457910a24ad237ae0e658b672b1
enwelum chiamaka <enwelumchiamaka@gmail.com>
2024-04-23 18:06:37 +0100

    Second setup of the app

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="flex"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
