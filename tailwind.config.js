f9847dbf35554877b5e60c63f505c8db5e1a71a5
enwelum chiamaka <enwelumchiamaka@gmail.com>
2024-04-23 19:08:38 +0100

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["montserrat", "sans-serif"],
+      },
+      color: {
+        primary:"#00C2FF",
+        secondary: "#DD0BFF",
+        dark: "#111111"
+      },
+      container: {
+        center: true,
+        padding: {
+          Default: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
   },
   plugins: [],
 }
