40e992b6bce5e2f02c0637b22486458d58b4c126
enwelum chiamaka <enwelumchiamaka@gmail.com>
2024-04-24 11:23:12 +0100

    Quotes section set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
+import Quotes from './Components/Quotes/Quotes';
 
 const App = () => {
   return (
-    <main className='overflow-x-hidden bg-white dark:bg-black'>
+    <main className='overflow-x-hidden bg-white dark:bg-black text-black dark:text-white duration-300'>
       <Navbar/>
       <Hero />
+      <Quotes />
     </main>
   )
 }
